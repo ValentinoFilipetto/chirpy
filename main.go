6e41b7d0501b8fa3100dc8d670b003815bc510e5
ValentinoFilipetto <valefilipetto@gmail.com>
2025-05-02 17:42:58 +0200

    feat: update fileserver path and add /healthz endpoint

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
    "fmt"
   )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+}  
+
 func main() {
    mux := http.NewServeMux()
 
-   mux.Handle("/", http.FileServer(http.Dir(".")))
+   fileServer := http.FileServer(http.Dir("."))   
+   mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+   mux.HandleFunc("/healthz", readinessHandler)
 
    server := &http.Server{
     Addr: "127.0.0.1:8080", // forces WSL2 to use IPv4
