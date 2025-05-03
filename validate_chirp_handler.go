0002bcdb2f33de87a3fe8ba4a77ff2e248977850
ValentinoFilipetto <valefilipetto@gmail.com>
2025-05-03 21:34:52 +0200

    feat: add clean body function

--- validate_chirp_handler.go
+++ validate_chirp_handler.go
@@ -1,60 +1,87 @@
 package main
 
 import (
-  "net/http"
-  "encoding/json"
-  "log"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
 )
 
+type errorVals struct {
+	Error string `json:"error"`
+}
+
+type returnVals struct {
+	Cleaned_body string `json:"cleaned_body"`
+}
+
+// Handler for JSON responses
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Printf("Error marshalling JSON")
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+// Handler for error responses
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	errorBody := errorVals{}
+	errorBody.Error = "Chirp is too long"
+	dat, err := json.Marshal(errorBody)
+
+	if err != nil {
+		log.Printf("Error marshalling JSON")
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func badWorldReplacement(str string) string {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(str, " ")
+
+	for i, word := range words {
+		for _, profaneWord := range profaneWords {
+			lowercaseWord := strings.ToLower(word)
+			if lowercaseWord == profaneWord {
+				words[i] = "****"
+			}
+		}
+	}
+
+	return strings.Join(words, " ")
+}
 
 func (cfg *apiConfig) ValidateChirpHandler(w http.ResponseWriter, r *http.Request) {
-   type parameters struct {
-       Body string `json:"body"`
-   }
+	type parameters struct {
+		Body string `json:"body"`
+	}
 
-   type returnVals struct {
-       Valid bool `json:"valid"`
-   }
+	respBody := returnVals{}
 
-   type errorVals struct {
-       Error string `json:"error"`
-   }
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
 
-   respBody := returnVals{}
-   errorBody := errorVals{}
+	w.Header().Set("Content-Type", "application/json")
 
-   decoder := json.NewDecoder(r.Body)
-   params := parameters{}
-   err := decoder.Decode(&params)
-   if err != nil {
-     log.Printf("Error decoding parameters: %s", err)
-     w.WriteHeader(500)
-     return
-   }
-
-   w.Header().Set("Content-Type", "application/json")
-
-   if len(params.Body) <= 140 {
-        respBody.Valid = true
-        dat, err := json.Marshal(respBody)
-
-        if err != nil {
-             log.Printf("Error marshalling JSON")
-             w.WriteHeader(500)
-             return
-        }
-        w.WriteHeader(200)
-        w.Write(dat)
-   } else {
-        errorBody.Error = "Chirp is too long"
-        dat, err := json.Marshal(errorBody)
-
-        if err != nil {
-                log.Printf("Error marshalling JSON")
-                w.WriteHeader(500)
-                return
-        }
-        w.WriteHeader(400)
-        w.Write(dat)
-   }
+	if len(params.Body) <= 140 {
+		params.Body = badWorldReplacement(params.Body)
+		respBody.Cleaned_body = params.Body
+		respondWithJSON(w, 200, respBody)
+	} else {
+		respondWithError(w, 400, "Chirp is too long")
+	}
 }
