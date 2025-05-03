0a045f7a3ee6f82d7cf40d4bd1d828a0aadcdbd4
ValentinoFilipetto <valefilipetto@gmail.com>
2025-05-03 11:14:19 +0200

    feat: add middleware to count number of requests

--- main.go
+++ main.go
@@ -3,7 +3,36 @@ package main
 import (
    "net/http"
    "fmt"
+   "sync/atomic"
   )
+
+type apiConfig struct {
+    fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+    // Get the current value
+    count := cfg.fileserverHits.Load()
+    
+    // Format it as a string
+    responseText := fmt.Sprintf("Hits: %d", count)
+    
+    // Write it to the response
+    w.Write([]byte(responseText))                                  
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+    cfg.fileserverHits.Store(0)
+    w.WriteHeader(http.StatusOK)
+}
+
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -13,10 +42,13 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
    mux := http.NewServeMux()
+   apiCfg := &apiConfig{}
 
    fileServer := http.FileServer(http.Dir("."))   
-   mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+   mux.Handle("/app/", apiCfg.middlewareMetrics(http.StripPrefix("/app", fileServer)))
    mux.HandleFunc("/healthz", readinessHandler)
+   mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+   mux.HandleFunc("/reset", apiCfg.resetMetricsHandler)
 
    server := &http.Server{
     Addr: "127.0.0.1:8080", // forces WSL2 to use IPv4
